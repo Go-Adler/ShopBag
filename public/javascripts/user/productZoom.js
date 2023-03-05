ebc986327fa333a74d3caa91f3e52ad7d2d73a65
Go-Adler <gokul_adler@outlook.com>
2023-03-06 00:34:27 +0530

    finding zoom

--- public/javascripts/user/productZoom.js
+++ public/javascripts/user/productZoom.js
@@ -1,20 +1,28 @@
 const imageContainer = document.querySelector('.card');
 const zoomContainer = document.querySelector('.product-image-zoom');
+const zoomContainerWidth = zoomContainer.offsetWidth;
+const zoomContainerHeight = zoomContainer.offsetHeight;
 
 imageContainer.addEventListener('mousemove', (event) => {
   const { left, top, width, height } = imageContainer.getBoundingClientRect();
   const x = event.clientX - left;
   const y = event.clientY - top;
-  const scaleX = zoomContainer.offsetWidth / width;
-  const scaleY = zoomContainer.offsetHeight / height;
-  const offsetX = (zoomContainer.offsetWidth / 2) - x * scaleX;
-  const offsetY = (zoomContainer.offsetHeight / 2) - y * scaleY;
+  const centerX = width / 2;
+  const centerY = height / 2;
+  const scaleX = zoomContainerWidth / width;
+  const scaleY = zoomContainerHeight / height;
+  const offsetX = (zoomContainerWidth / 2) - x * scaleX;
+  const offsetY = (zoomContainerHeight / 2) - y * scaleY;
+  const invertX = x > centerX ? -1 : 1;
+  const invertY = y > centerY ? -1 : 1;
 
   zoomContainer.style.display = 'block';
   zoomContainer.style.backgroundImage = `url(${event.target.src})`;
-  zoomContainer.style.backgroundPosition = `${x}px ${y}px`;
+  zoomContainer.style.backgroundPosition = `${invertX * (x - centerX)}px ${invertY * (y - centerY)}px`;
   zoomContainer.style.backgroundSize = `${width}px ${height}px`;
   zoomContainer.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+  zoomContainer.style.top = `${top - zoomContainerHeight - 10}px`;
+  zoomContainer.style.left = `${left + width / 2 - zoomContainerWidth / 2}px`;
 });
 
 imageContainer.addEventListener('mouseleave', () => {
