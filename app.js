3d5ec26670769b5f455926df477747ff18e83160
Go-Adler <gokul_adler@outlook.com>
2023-04-25 17:47:33 +0530

    fixed mongo

--- app.js
+++ app.js
@@ -21,10 +21,6 @@ app.set('view engine', 'ejs')
 
 app.use(morgan("dev"))
 app.use(nocache())
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`)
-  next()
-})
 app.use(express.static('./public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
