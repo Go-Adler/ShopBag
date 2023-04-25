52b93e3787eb80d62b5db9146d67ec3c697e31f2
Go-Adler <gokul_adler@outlook.com>
2023-04-25 07:29:16 +0530

    fixed mongo

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose"
+import { config } from 'dotenv'
+
+config({ path: "./services/userServices/.env" })
 
 mongoose.set('strictQuery', true)
 
 export const mongo = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/shop_bag')
+        const mongoPassword = process.env.MONGO_PASS
+        await mongoose.connect(`mongodb+srv://goadler:${mongoPassword}@cluster0.nhchirs.mongodb.net/Shop_Bag`)
     } catch {
         throw new Error(`Error: ${error.message}`);
     }
