35e3d10605a22714c06deb3f494c68a151f83f16
Go-Adler <gokul_adler@outlook.com>
2023-04-25 13:16:40 +0530

    fixed mongo

--- config/mongoose.js
+++ config/mongoose.js
@@ -9,7 +9,7 @@ export const mongo = async () => {
     try {
         const mongoPassword = process.env.MONGO_PASS
         await mongoose.connect(`mongodb+srv://goadler:${mongoPassword}@cluster0.nhchirs.mongodb.net/Shop_Bag`)
-    } catch {
+    } catch (error) {
         throw new Error(`Error: ${error.message}`);
     }
 }
